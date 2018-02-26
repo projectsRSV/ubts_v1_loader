4f7a7cbcdfe008799f487a31bacdbc21f01c9da7
projectsRSV <rsvukraine@gmail.com>
2018-02-26 10:19:05 +0200

    correct clear interrupt

--- UBTS_MB/ISR.c
+++ UBTS_MB/ISR.c
@@ -13,7 +13,7 @@ void ISR_init(void){
 ISR(PORTF_INT0_vect){
 	uint8_t ch,interReg,socketInt;
 
-blinkFuncPtr = (fpStatusLed)(blinkPtrTable[1]);				//speed of led blinking
+blinkFuncPtr = (fpStatusLed)(blinkPtrTable[0]);				//speed of led blinking
 
 	interReg = w5200_readInterChann()&0x01;
 	while(interReg){
@@ -21,7 +21,7 @@ blinkFuncPtr = (fpStatusLed)(blinkPtrTable[1]);				//speed of led blinking
 		socketInt=w5200_readSocketInt(ch);
 		switch (socketInt) {
 			case 0x02:{														//disconnect
-				w5200_writeByte(Sn_IR(ch),0xff);
+				w5200_writeByte(Sn_IR(ch), socketInt);
 				w5200_discSocket(ch);
 				w5200_openSocket(ch);
 				break;
@@ -30,12 +30,12 @@ blinkFuncPtr = (fpStatusLed)(blinkPtrTable[1]);				//speed of led blinking
 				uint16_t buffLength;
 				buffLength=w5200_recvData(MAIN_CH,buffer);
 				//usart_sendData_toAscii(&USARTE0,buffLength,buffer);			//logging
-				loaderAction=commandDecoder(buffLength,buffer);
-				w5200_writeByte(Sn_IR(ch),0xff);
+				loaderAction = commandDecoder(buffLength,buffer);
+				w5200_writeByte(Sn_IR(ch), socketInt);
 				break;
 			}
 			default:{
-				w5200_writeByte(Sn_IR(ch),0xff);
+				w5200_writeByte(Sn_IR(ch), socketInt);
 				break;
 			}
 		}
